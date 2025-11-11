39304584ba8f0b9763ba1af3093167eb9d04f21d
tshavit2011-bit <tshavit2011@gmail.com>
2025-11-11 20:29:11 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,11 +1,6 @@
 def approximate_pi(n_terms):
-
-leibniz_series = []
-
-for i in range(n_terms):
-
-thing = (-1)**i/(2*i+1)
-
-leibniz_series.append(thing)
-
-return (sum(leibniz_series) *4)
+ leibniz_series = []
+ for i in range(n_terms):
+     thing = (-1)**i/(2*i+1)
+     leibniz_series.append(thing)
+ return (sum(leibniz_series) *4)
